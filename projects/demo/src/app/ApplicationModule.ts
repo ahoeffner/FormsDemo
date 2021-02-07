18ed18547b11a17077f62b2de1dfaae79bb4298e
Alex Høffner <alex@hoeffner.net>
2021-02-07 21:59:54 +0100

    .

--- projects/demo/src/app/ApplicationModule.ts
+++ projects/demo/src/app/ApplicationModule.ts
@@ -1,25 +1,58 @@
-import { NgModule } from '@angular/core';
+import { NgModule,ComponentRef } from '@angular/core';
 import { ApplicationRoot } from './ApplicationRoot';
 import { MaterialModules } from './MaterialModules';
 import { BrowserModule } from '@angular/platform-browser';
+import { Application, FormsLibrary, FormsDefinition, Builder } from 'm42forms';
 
 // Formsdefinitions
 
 import { Test1 } from './Forms/Test1/Test1';
+import { Test2 } from './Forms/Test2/Test2';
+
+
+const forms:FormsDefinition[] =
+[
+    {name: "test1", title: "Test1", component: Test1},
+    {name: "test2", title: "Test2", component: Test2}
+];
 
 
 
 @NgModule({
   declarations: [
     ApplicationRoot,
-    Test1
+    Test1, Test2
   ],
   imports: [
     BrowserModule,
-    MaterialModules
+    MaterialModules,
+    FormsLibrary
   ],
   providers: [],
   bootstrap: [ApplicationRoot]
 })
 
-export class ApplicationModule { }
+
+export class ApplicationModule
+{
+    form:string = "test1";
+
+    constructor(private app:Application)
+    {
+        app.title = "Demo";
+        app.setFormsDefinitions(forms);
+        //app.showform("test1");
+        //this.showform();
+        setTimeout(() => this.showform(),1000);
+      }
+
+    public showform() : void
+    {
+      this.app.showform(this.form);
+
+      if (this.form == "test1") this.form = "test2";
+      else this.form = "test1";
+
+      //setTimeout(function() {this.showform()},10000);
+    }
+}
--- projects/demo/src/app/Forms/Test2/Test2.ts
+++ projects/demo/src/app/Forms/Test2/Test2.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'test1',
+    selector: 'test2',
     template:
     `
-      Hello from Test1 VII {{n}} <button (click)="add()">Test1</button>
+      Hello from Test2 VII {{n}} <button (click)="add()">Test1</button>
     `,
     styleUrls: []
   })
 
 
-  export class Test1
+  export class Test2
   {
     public n:number = 1;
 
