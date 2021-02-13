70e82fc8c77b5380ddfddf810852fcc090ccdae1
Alex Høffner <alex@hoeffner.net>
2021-02-13 23:21:40 +0100

    Update Test2.ts

--- projects/demo/src/app/Forms/Test2/Test2.ts
+++ projects/demo/src/app/Forms/Test2/Test2.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
     selector: 'test2',
     template:
     `
-      Test2 <button (click)="add()">Up</button>
+      Test2 {{n}} <button (click)="add()">Up</button>
     `,
 	styleUrls: []
 })
@@ -19,6 +19,5 @@ export class Test2 extends Form
 	public add()
 	{
 		this.n++;
-		window.alert("n = "+this.n);
 	}
 }
