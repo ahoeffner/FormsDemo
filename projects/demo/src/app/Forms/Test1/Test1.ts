058ed34f56b5d098cdf2355142755c6bc079b786
Alex Høffner <alex@hoeffner.net>
2021-02-16 23:05:15 +0100

    Update Test1.ts

--- projects/demo/src/app/Forms/Test1/Test1.ts
+++ projects/demo/src/app/Forms/Test1/Test1.ts
@@ -1,7 +1,7 @@
 import { Block1 } from './/Block1';
 import { Test2 } from '../Test2/Test2';
 import { Component } from '@angular/core';
-import { Form, FormBlock } from 'm42forms';
+import { Form, BLOCK } from 'm42forms';
 
 
 @Component({
@@ -12,7 +12,7 @@ import { Form, FormBlock } from 'm42forms';
       Test1 {{n}} <button (click)="add()">Up</button>
       <button (click)="callForm('test2')">Test2</button>
       <button (click)="close()">Close</button>
-      <button (click)="block1.sayHello('World')">Hello</button>
+      <button (click)="emp.sayHello('World')">Hello</button>
       </div>
     `,
     styleUrls: []
@@ -23,14 +23,14 @@ import { Form, FormBlock } from 'm42forms';
 export class Test1 extends Form
 {
   public n:number = 1;
-  @FormBlock("emp") public empblock:Block1;
+  @BLOCK("emp") public emp:Block1 = new Block1();
 
 
   constructor()
   {
     super();
-    this.empblock = new Block1();
     this.setCallback(this.callback);
+    console.log("Hello I");
   }
 
 	public add()
