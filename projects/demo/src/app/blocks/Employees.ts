f878d7005077e1dd2df74de45a2c301d80684074
Alex Høffner <alex@hoeffner.net>
2021-03-20 00:14:26 +0100

    Update Employees.ts

--- projects/demo/src/app/blocks/Employees.ts
+++ projects/demo/src/app/blocks/Employees.ts
@@ -3,10 +3,10 @@ import { DATABASE, BLOCK, FIELD, Block } from "forms42";
 @BLOCK("emp")
 @DATABASE({query:false, insert:true, update: true, delete: true})
 
-@FIELD("id","input")
-@FIELD("job","input")
-@FIELD("firstname","input")
-@FIELD("lastname","input")
+@FIELD({name: "id", type: "input"})
+@FIELD({name: "job", type: "input"})
+@FIELD({name: "firstname", type: "input"})
+@FIELD({name: "lasttname", type: "input"})
 
 export class Employees extends Block
 {
