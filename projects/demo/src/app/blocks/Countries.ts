97b8e038c52a74cdf1216074d3b4e64656637917
Alex Høffner <alex@hoeffner.net>
2021-04-02 11:59:35 +0200

    .

--- projects/demo/src/app/blocks/Countries.ts
+++ projects/demo/src/app/blocks/Countries.ts
@@ -1,38 +1,16 @@
-import { database, alias, field, Block, table, column, key, orderby, FieldType, FieldTriggerEvent, FieldTrigger, Case } from "forms42";
+import { alias, Block, table, column, key, orderby, Case } from "forms42";
 
 
 @alias("country")
+
 @table("countries")
 @orderby("country_id")
 
-@column({name: "country_id"  , type: "varchar"})
-@column({name: "country_name", type: "varchar"})
+@column({name: "country_id"  , type: "varchar", mandatory: true, case: Case.upper})
+@column({name: "country_name", type: "varchar", mandatory: true})
 
 @key("primary",true,"country_id")
 
-@field({name: "code"    , column: "country_id", mandatory: true, case: Case.upper})
-@field({name: "country" , column: "country_name", mandatory: true})
-
-
-@database({query:true})
 export class Countries extends Block
 {
-    constructor()
-    {
-        super();
-        this.addFieldTrigger(this.setName,FieldTrigger.PostChange,["code","country"]);
-    }
-
-
-    public async setName(event:FieldTriggerEvent) : Promise<boolean>
-    {
-        let code:string = this.getValue("code",event.row);
-        let country:string = this.getValue("country",event.row);
-
-        if (code == null) code = "";
-        if (country == null) country = "";
-
-        this.setValue("name",event.row,country+" ("+code+")");
-        return(true);
-    }
 }
